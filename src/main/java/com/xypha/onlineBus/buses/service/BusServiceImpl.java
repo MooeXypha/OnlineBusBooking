46aabb0c0848d26f39c0510ed4ce4bd918e009af
MooeXypha <bhonemyat.mooe@xyphatech.com>
2025-12-16 10:16:50 +0630

    paginate final parrt

--- src/main/java/com/xypha/onlineBus/buses/service/BusServiceImpl.java
+++ src/main/java/com/xypha/onlineBus/buses/service/BusServiceImpl.java
@@ -27,11 +27,11 @@ public class BusServiceImpl implements BusService {
 
     @Override
     public ApiResponse<PaginatedResponse<BusResponse>> getBusesPaginatedResponse(int offset, int limit) {
-        int offsets = offset * limit;
-        List<Bus> buses = busMapper.findPaginated(offsets, limit);
+//        int offsets = offset * limit;
+        List<Bus> buses = busMapper.findPaginated(offset, limit);
         List<BusResponse> responseList = buses.stream().map(this::mapToResponse).collect(Collectors.toList());
         int total = busMapper.countBuses();
-        PaginatedResponse<BusResponse> paginatedResponse = new PaginatedResponse<>(offsets, limit, total, responseList);
+        PaginatedResponse<BusResponse> paginatedResponse = new PaginatedResponse<>(offset, limit, total, responseList);
         return new ApiResponse<>("SUCCESS", "Buses retrieved successfully", paginatedResponse);
     }
 
